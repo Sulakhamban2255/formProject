fb1fad4ede618adb129e8f9ab44caefa1bb46ba1
sulakh1993 <sulakh281293@gmail.com>
2024-02-02 17:39:08 +0530

    first commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginData = {
+    username: '',
+    password: ''
+  };
+
+  login() {
+    // Add your login logic here
+    console.log('Login clicked with:', this.loginData);
+  }
+}
